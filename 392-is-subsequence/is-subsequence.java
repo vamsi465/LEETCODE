2b1b816af0296a8c4e942f72e39bf6941a4c7621
vamsi465 <vamsikrishnasiriveri05@gmail.com>
2026-08-10 09:55:59 +0530

    Time: 2 ms (73.89%) | Memory: 42.7 MB (63.24%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+      int j=0;
+      if(s.equals(""))
+      {
+        return true;
+      }
+      {
+
+      }
+      for(int i=0;i<t.length();i++)
+      {
+        if(s.charAt(j)==t.charAt(i))
+        {
+            j++;
+        }
+        if(j==s.length())
+        {
+            return true;
+        }
+      }  
+      return false;
+    }
+}
